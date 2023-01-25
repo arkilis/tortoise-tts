c93844f15026486609a250ad2f540dbd52057ca4
Ben Liu <arkilis@gmail.com>
2023-01-26 09:06:47 +1000

    update speech.py

--- speech.py
+++ speech.py
@@ -1,3 +1,5 @@
+import uuid
+import os
 import time
 import torch
 import torchaudio
@@ -5,12 +7,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tortoise.api import TextToSpeech
 from tortoise.utils.audio import load_audio, load_voice, load_voices
-import uuid
 
 # start time
 start_time = time.time()
 
 # This will download all the models used by Tortoise from the HuggingFace hub.
+
 useCPU = False
 DEVICE = torch.device('cuda:0' if (torch.cuda.is_available() and not useCPU) else 'cpu')
 print('Using device:', DEVICE)
@@ -21,34 +23,6 @@ if not useCPU:
 
 tts = TextToSpeech(device=DEVICE)
 
-# Here's something for the poetically inclined.. (set text=)
-text = """
-Once upon a time, in a faraway land, there lived a kind and gentle king who ruled over a beautiful and prosperous kingdom. The king was known for his wisdom and fairness, and his people loved and respected him.
-<break>
-One day, the king decided to go on a journey to visit all the corners of his kingdom and make sure that everyone was happy and healthy. He traveled from town to town, from village to village, and from farm to farm, listening to the concerns of his people and doing everything he could to help them.
-<break>
-As the king was traveling, he came across a small cottage in the woods. The cottage was old and run-down, and it looked as if no one had lived there for a very long time. But as the king approached the door, he heard a faint sound coming from inside.
-<break>
-He knocked on the door and called out, Hello, is anyone there?
-<break>
-To his surprise, a tiny voice answered, Yes, Your Majesty. Please come in.
-<break>
-The king opened the door and entered the cottage. Inside, he found a small, old woman sitting by the fireplace. She was so tiny that she barely reached up to the king's knee.
-<break>
-How long have you lived here all by yourself? the king asked the woman.
-<break>
-Oh, I don't know, the woman replied. It feels like forever. I'm all alone, and I don't have anyone to talk to or take care of me.
-<break>
-The king felt sorry for the woman, and he decided to help her. He ordered his men to fix up the cottage and make it warm and comfortable. He also sent some of his best cooks to prepare delicious meals for the woman and make sure she had everything she needed.
-<break>
-The woman was overjoyed by the king's kindness. She thanked him and promised to be forever grateful.
-<break>
-The king continued his journey and visited many more people in need. He helped the sick, the poor, and the lonely, and everywhere he went, he was greeted with smiles and thanks.
-<break>
-Finally, after many months, the king returned to his palace. He was tired but happy, knowing that he had made a difference in the lives of his people.
-<break>
-And from that day on, the king was known not only for his wisdom and fairness, but also for his kindness and generosity. His kingdom flourished, and everyone lived happily ever after.
-"""
 
 def generate(file_name, text):
     # Pick a "preset mode" to determine quality. Options: {"ultra_fast", "fast" (default), "standard", "high_quality"}. See docs in api.py
@@ -63,6 +37,29 @@ def generate(file_name, text):
     torchaudio.save(file_name, gen.squeeze(0).cpu(), 24000)
     print("--- {} seconds: {} ---".format("%0.2f" % (time.time() - start_time), file_name))
 
-file_name = str(uuid.uuid4())
-for index, p in enumerate(text.split("<break>")):
-    generate(file_name+"_"+str(index)+".wav", p)
+
+SOURCE_PATH = "stories"
+SOURCE_FILE = "source.txt"
+def generate_stories():
+    lines = [] # final lines will be spoken
+
+    dirs = os.listdir(os.path.join(os.getcwd(), SOURCE_PATH))
+    for dir in dirs:
+        dir_full_path = os.path.join(os.getcwd(), SOURCE_PATH, dir)
+        if os.path.isdir(dir_full_path):
+            file_full_path = os.path.join(dir_full_path, SOURCE_FILE)
+            if os.path.exists(file_full_path) and "done_" not in file_full_path:
+                print("reading story from dir [" + dir + " ]...")
+                lines = open(file_full_path).readlines()
+                lines = list( map(lambda x:x.strip(), lines))
+                lines = [x for x in lines if x]
+                # print(lines)
+
+                
+                file_name = str(uuid.uuid4())
+                for index, line in enumerate(lines):
+                    voice_file = os.path.join(dir_full_path, file_name+"_"+str(index)+".wav")
+                    print("saved file name: " + voice_file)
+                    generate(voice_file, line)
+
+generate_stories()
